42bce98fffd48a9c7a853585f7c295af39060c89
jacksweat <jacksweat93@outlook.com>
2018-06-24 22:43:28 +0800

    first commit

---
+++ app_server/controllers/main.js
@@ -0,0 +1,3 @@
+module.exports.index = function(req, res){
+ res.render('index', { title: 'My name is Jacksweat. I am looking for genious method' ,title1:'The one who sows winds harvests storms' });
+};
